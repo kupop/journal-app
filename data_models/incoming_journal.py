a33ce70b12893cd23cd59213cdbe933264e2bd7b
kupop <lei.mander@gmail.com>
2024-04-23 11:54:59 +0200

    adjust property casing

--- data_models/incoming_journal.py
+++ data_models/incoming_journal.py
@@ -1,8 +1,8 @@
 from typing import datetime
 
 class IncomingJournal:
-  def __init__(self, socialSecuritynumber: int, documentType: str, version: int, timeStamp : datetime, savedTimeStamp: datetime):
-    self.socialSecuritynumber = socialSecuritynumber
+  def __init__(self, socialSecurityNumber: int, documentType: str, version: int, timeStamp : datetime, savedTimeStamp: datetime):
+    self.socialSecuritynumber = socialSecurityNumber
     self.documentType = documentType
     self.version = version
     self.timeStamp = timeStamp
