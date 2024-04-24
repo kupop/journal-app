542091636382879346f8d03f04e7248f05339cea
kupop <lei.mander@gmail.com>
2024-04-24 17:50:52 +0200

    rename data_models to models

--- data_models/persisted_journal.py
+++
@@ -1,8 +0,0 @@
-from typing import datetime, uuid
-
-class PersistedJournal:
-  def __init__(self, patientId: uuid, documentType: str, version: int, savedTimeStamp: datetime):
-    self.patientId = patientId
-    self.documentType = documentType
-    self.version = version
-    self.savedTimeStamp = savedTimeStamp
