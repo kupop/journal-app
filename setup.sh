8f2a4bfb6dff60dc4470b34677094114201b2a98
kupop <lei.mander@gmail.com>
2024-04-25 11:26:28 +0200

    changed path to db_setup

--- setup.sh
+++ setup.sh
@@ -4,4 +4,4 @@ python -m venv venv
 source venv/bin/activate
 docker-compose -f config/docker-compose.yml up -d
 pip install -r requirements.txt
-PYTHONPATH=. python3 database/db_client.py
+PYTHONPATH=. python3 db_setup.py
