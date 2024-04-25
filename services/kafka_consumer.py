15987c5a8205f187cf942b3368be277b14ce1859
kupop <lei.mander@gmail.com>
2024-04-25 11:29:50 +0200

    call db_client from kafka_consumer

--- services/kafka_consumer.py
+++ services/kafka_consumer.py
@@ -1,11 +1,14 @@
 import json
 from kafka import KafkaConsumer
 
+from database.db_client import persist_patient_record
+
 if __name__ == '__main__':
 
     consumer = KafkaConsumer(
+        "test",
         bootstrap_servers='localhost:9092',
         auto_offset_reset='earliest'
     )
     for message in consumer:
-        print(json.loads(message.value))
+        persist_patient_record(json.loads(message.value))
